1ffab2d01211e680f564653d9ab7b14a2c4cc293
Lucas Labs <lucascaladob@gmail.com>
2024-04-02 20:06:33 -0300

    feat: player scene and script

---
+++ DeepWoods/script/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+var speed = 100
+
+var playerState
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left","right","up","down");
+	
+	if direction.x == 0 and direction.y == 0 :
+		playerState = "idle"
+	
+	elif  direction.x != 0 or direction.y != 0 :
+		playerState = "walking"
+	
+	velocity = direction * speed
+	move_and_slide()
+
+	play_anim(direction)
+	
+func play_anim(dir):
+	if playerState == "idle":
+		$AnimatedSprite2D.play("idle");
+
+	if playerState == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("n-walk")
+		if dir.x == 1:
+			$AnimatedSprite2D.play("e-walk")
+		if dir.y == 1:
+			$AnimatedSprite2D.play("s-walk")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("w-walk")
+		
+		if dir.x > 0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("ne-walk")
+		if dir.x > 0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("se-walk")
+		if dir.x < -0.5 and dir.y > 0.5:
+			$AnimatedSprite2D.play("sw-walk")
+		if dir.x < -0.5 and dir.y < -0.5:
+			$AnimatedSprite2D.play("nw-walk")
+
+func  player():
+	pass
+		
+		

5d02e0b6fc2eb6b1da3bdaf70634ee75d66f286d
Lucas Labs <lucascaladob@gmail.com>
2024-04-09 22:09:57 -0300

    feat: add inventory for the player

--- DeepWoods/script/player.gd
+++ DeepWoods/script/player.gd
@@ -4,6 +4,8 @@ var speed = 100
 
 var playerState
 
+@export var inventory: Inventory
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down");
 	
@@ -43,5 +45,3 @@ func play_anim(dir):
 
 func  player():
 	pass
-		
-		

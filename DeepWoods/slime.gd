93d61eaf72fd39e5908ee08ed3f7050d3eed6a12
Lucas Labs <lucascaladob@gmail.com>
2024-04-14 19:22:03 -0300

    feat: create new enemy

---
+++ DeepWoods/slime.gd
@@ -0,0 +1,75 @@
+extends CharacterBody2D
+
+var speed = 55
+
+var health = 100
+
+var dead = false
+
+var playerInArea = false
+
+var player
+
+@onready var slime = $slime_collctable
+@export var itemRes: InvItem 
+
+func _physics_process(delta):
+	if !dead:
+		$detection_area/CollisionShape2D.disabled = false
+		if playerInArea:
+			position += (player.position - position) / speed
+			$AnimatedSprite2D.play("move")
+		else:
+			$AnimatedSprite2D.play("idle")
+	if dead:
+		$detection_area/CollisionShape2D.disabled = true
+
+
+
+
+func OnDetectionAreaBodyEntered(body):
+	if body.has_method("player"):
+		playerInArea = true
+		player = body
+
+
+func OnDetectionAreaBodyExited(body):
+	if body.has_method("player"):
+		playerInArea = false
+		player = body
+
+
+func OnHitboxAreaEntered(area):
+	var damage = 0
+	
+	if area.has_method("arrow_deal_damage"):
+		damage = 25
+		take_damage(damage)
+		
+func take_damage(damage):
+	health -= damage
+	
+	if health <= 0 and !dead:
+		death()
+
+func death():
+	dead = true
+	$AnimatedSprite2D.play("death")
+	await get_tree().create_timer(1).timeout
+	drop_slime()
+	
+	$AnimatedSprite2D.visible = false
+	$hitbox/CollisionShape2D.disabled = true
+	$detection_area/CollisionShape2D.disabled = true
+
+
+func drop_slime():
+	slime.visible = true
+	slime_collect()
+	
+
+func slime_collect():
+	await get_tree().create_timer(1.5).timeout
+	slime.visible = false
+	player.collect(itemRes)
+	queue_free()
